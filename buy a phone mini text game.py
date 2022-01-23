f700bade6a1935232554cddc99f22d637de54f4a
Abir Bin Saleh <89151452+Abir4123@users.noreply.github.com>
2022-01-23 23:02:26 +0600

    Add files via upload

---
+++ buy a phone mini text game.py
@@ -0,0 +1,23 @@
+a = int(input("Type in a number: "))
+
+
+if a > 1000:
+    print("WOW!! now you got " + str(a), "amount of taka in your account now you can buy a phone!!")
+else:
+    print("Sorry you cant buy a phone now :(")
+
+b=input("press any key to buy a phone: ")
+
+
+
+buyaphone = int(input("SHOPKEEPER:How much taka do you have?"))
+costofaphone = 5000
+
+if buyaphone >  costofaphone:
+    print("congrats you have a phone now now you dont need to use your moms phone anymore yay!!")
+else:
+    print("sad bro you need more money to buy the phone  :(")
+
+exit()
+
+print("TRY AGAIN!")
